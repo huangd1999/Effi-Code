4012e16c060293d2ff4f5638bd9e88a189455bc8
huangd179 <u3008427@connect.hku.hk>
2024-10-02 11:22:49 +0800

    Add LLaMA-Factory as a regular directory

---
+++ LLaMA-Factory/run.sh
@@ -0,0 +1,4 @@
+llamafactory-cli train examples/train_full/DeepSeek-Coder-6.7B-base.yaml
+llamafactory-cli train examples/train_full/DeepSeek-Coder-6.7B-instruct.yaml
+llamafactory-cli train examples/train_full/Qwen2.5-7B-Code.yaml
+llamafactory-cli train examples/train_full/Qwen2.5-7B-Coder-Instruct.yaml
